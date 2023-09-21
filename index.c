5ad27ebd6027ecc9aa706e075bcf6b89e921449f
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-09-21 15:28:54 -0300

    arquivo do projeto
    projeto criado em C#

---
+++ index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Esta é a função principal do programa
+    // Imprime a mensagem "Olá, mundo!" no console
+    printf("Olá, mundo!\n");
+
+    // Retorna 0 para indicar que o programa terminou com sucesso
+    return 0;
+}
